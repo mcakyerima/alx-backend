46e8922105551fc008eb3aa4e259e6328c95e529
mcakyerima <mcakyerima@gmail.com>
2024-04-04 04:32:05 +0100

    ✅ Finished task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" FIFOCache class that inherits from BaseCaching"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    """ Inherits from BaseCaching """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Assign to dictionary self.cache_data """
+        if key is None or item is None:
+            return
+        if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+            discard, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {discard}")
+
+        self.cache_data[key] = item
+
+
+    def get(self, key):
+        """ returns item that is linked to key in cache_data"""
+        return self.cache_data.get(key, None)
